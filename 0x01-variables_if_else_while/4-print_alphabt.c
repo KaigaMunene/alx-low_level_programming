0a8e15b2be4bc7ccebde7e8434a47b0b9a4e49d4
Super User <kaigamunene@gmail.com>
2022-06-06 20:24:23 -0700

    alphabet in lowercase, don't print q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != e && l != q)
+			putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
