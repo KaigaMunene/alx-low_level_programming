71f31b8aca0ad8fc18728e98d98f6e057fcea695
Super User <kaigamunene@gmail.com>
2022-06-06 19:45:37 -0700

    changes

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	char ch;
+char ch;
 
-	 for (ch= 'a'; ch<= 'z'; ch++)
-                  putchar(ch);
-                  putchar ('\n');
+for (ch= 'a'; ch<= 'z'; ch++)
+	putchar(ch);
+	putchar ('\n');
 
-         return (0);
+	return (0);
 }
