90e5faabce9eb06fb8dcdec38b0c53568661d9b6
Super User <kaigamunene@gmail.com>
2022-06-13 03:10:39 -0700

    print digits 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Print digits from 0-9, followed by newline
+ *
+ * Return:       None
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = 0; c <  9; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
