66a29e10423fc9e6dfa1fcd7fb1fc3eb2ff9fb03
Super User <kaigamunene@gmail.com>
2022-06-21 02:23:48 -0700

    function that fills memory with a constatnt byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * _memset - function that copies memory area
  *
  * @s: the address of memory to print
- * @b: 
+ * @b: constant byte
  * @n: size of the memory to print
  *
  * Return: a pointer to the memory area s
@@ -12,5 +12,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
+unsigned int i;
 
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
 }
