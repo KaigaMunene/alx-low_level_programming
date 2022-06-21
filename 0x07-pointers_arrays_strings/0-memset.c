b219019cca9676b7fda61f1af2f5984142b3ae2b
Super User <kaigamunene@gmail.com>
2022-06-21 02:06:53 -0700

    create file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memset - function that copies memory area
+ *
+ * @s: the address of memory to print
+ * @b: 
+ * @n: size of the memory to print
+ *
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# 0x07. C - Even more pointers, arrays and strings
+
+0-memset.c - function that fills memory with a constant byte
+* The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b
+* Returns a pointer to the memory area s
+
