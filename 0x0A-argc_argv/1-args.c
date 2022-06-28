7ab41751f82b2e1891fe062cdae5d1a28666bd62
Super User <kaigamunene@gmail.com>
2022-06-28 11:46:34 -0700

    removing trailling white space

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
 /**
  * main - a program that prints the number of arguments passed into it
  *
- * @argc: numbers of arguments passed 
+ * @argc: numbers of arguments passed
  * @argv: array of argements passed
  *
  * Return: Always 0 (Success)
