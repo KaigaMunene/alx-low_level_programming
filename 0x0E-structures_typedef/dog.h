9aa247a02c8d19073853f0318c52ea625779f217
Super User <kaigamunene@gmail.com>
2022-07-06 03:00:31 -0700

    create new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,5 +23,7 @@ void print_dog(struct dog *d);
  * dog_t -typedef for struct dog
  */
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
