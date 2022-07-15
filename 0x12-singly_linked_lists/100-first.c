e3d560a2dc292d65396de28d7608dd523bb341f5
KaigaMunene <kaigamunene@gmail.com>
2022-07-15 09:46:56 +0300

    print the following string

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * on_start   - Prints the string "Starting...\n"
+ *
+ */
+
+void on_start(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
