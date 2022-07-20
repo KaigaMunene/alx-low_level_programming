c1a282e6e438fe683c2fbfc320d43aabb662b212
KaigaMunene <kaigamunene@gmail.com>
2022-07-20 06:04:19 +0300

    fix header prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,6 +30,6 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int pop_listint(listint_t **head);
 int pop_listint_at_index(listint_t **head, unsigned int idx);
-int delete_nodeint_at_index(listint_t **head, unsigned int idx);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
