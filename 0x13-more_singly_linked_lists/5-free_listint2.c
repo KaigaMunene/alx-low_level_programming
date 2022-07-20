5793f404ba9848efed5c1a052caf2c51c65177e9
KaigaMunene <kaigamunene@gmail.com>
2022-07-20 05:46:38 +0300

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a listint_t list
+ * @head: head of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if (head == NULL)
+return;
+while (*head != NULL)
+{
+tmp = *head;
+*head = (*head)->next;
+free(tmp);
+}
+}
