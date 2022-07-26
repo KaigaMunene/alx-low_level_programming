eca504f8a7a8519b4930feab029a7475ee3cf5b3
KaigaMunene <kaigamunene@gmail.com>
2022-07-26 12:32:44 +0300

    redo the function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,31 +10,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
+ssize_t o, r, w;
 char *buffer;
-ssize_t bytes_read;
-ssize_t bytes_written;
 
 if (filename == NULL)
-return (-1);
-
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (-1);
+return (0);
 
 buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
-return (-1);
+return (0);
 
-bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1)
-return (-1);
+o = open(filename, O_RDONLY);
+r = read(o, buffer, letters);
+w = write(STDOUT_FILENO, buffer, r);
 
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-if (bytes_written == -1)
-return (-1);
+if (o == -1 || r == -1 || w == -1 || w != r)
+{
+free(buffer);
+return (0);
+}
 
 free(buffer);
-close(fd);
-return (bytes_read);
+close(o);
+
+return (w);
 }
